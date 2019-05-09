9d8c23f1683ab9ae170bb5b380acf71ae9dd5086
Jim Hessin <jhessin@gmail.com>
2019-05-09 13:43:36 -0600

    Added a case insensitive version.

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
@@ -6,11 +7,40 @@ mod tests;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    for line in search(&config.query, &contents) {
-        println!("{}",line);
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
     }
 
     Ok(())
+}
+
+#[derive(Debug, PartialOrd, PartialEq)]
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+            case_sensitive,
+        })
+    }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -24,20 +54,14 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-#[derive(Debug, PartialOrd, PartialEq)]
-pub struct Config {
-    pub query: String,
-    pub filename: String,
-}
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
 
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
         }
-        Ok(Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
-        })
     }
+    results
 }
--- src/lib/tests.rs
+++ src/lib/tests.rs
@@ -15,25 +15,61 @@ fn one_result() {
     assert_eq!(vec!["safe, fast, productive."], search(query, contents));
 }
 
+#[test]
 fn no_results() {
     let (query, contents) = setup_query("bob");
 
     assert_eq!(Vec::<&str>::new(), search(query, contents));
 }
 
+#[test]
 fn multiple_results() {
     let (query, contents) = setup_query("st");
 
-    assert_eq!(vec!["Rust:", "safe, fast, productive."], search(query, contents));
+    assert_eq!(
+        vec!["Rust:", "safe, fast, productive."],
+        search(query, contents)
+    );
 }
 
 #[test]
 fn config_read() {
-    let input = [" ".to_owned(), "Something".to_owned(), "Somefile.txt".to_owned()];
+    let input = [
+        " ".to_owned(),
+        "Something".to_owned(),
+        "Somefile.txt".to_owned(),
+    ];
 
     let query = input[1].clone();
     let filename = input[2].clone();
+    let case_sensitive = true;
 
+    assert_eq!(Ok(Config { query, filename, case_sensitive }), Config::new(&input))
+}
 
-    assert_eq!(Ok(Config { query, filename}), Config::new(&input))
+#[test]
+fn case_sensitive() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+}
+
+#[test]
+fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(
+        vec!["Rust:", "Trust me."],
+        search_case_insensitive(query, contents)
+    );
 }
